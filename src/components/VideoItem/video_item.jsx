004a931bdf4ce7c7a90a8802172f7f987e88556a
kimminsub <msmk530@naver.com>
2020-10-19 20:25:42 +0900

    [feat] 기본 layout
    한줄에 2개의 컨텐츠 보이도록 css 작성
    
    postCSS의 형식에 맞추어 작성

--- src/components/VideoItem/video_item.jsx
+++ src/components/VideoItem/video_item.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styles from './styles/video_item.module.css';
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video: { snippet } }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt='video thumbnail'
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VideoItem;
--- src/components/VideoList/video_list.jsx
+++ src/components/VideoList/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../VideoItem/video_item';
+import styles from './styles/video_list.module.css';
 
 const VideoList = (props) => (
-  <ul>
+  <ul className={styles.videos}>
     {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
