33da619e515d76dfa2c6898c84cf055318718d99
kimminsub <msmk530@naver.com>
2020-10-19 19:03:42 +0900

    [feat] yotube api를 사용하여 most popular 영상 25개 받아오기
    postman을 통해 fetch url만들고 code를 가져왔다.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 import './app.css';
 
 function App() {
-  return <h1>hello :)</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCYDQM2sTPSYaXL6l-VNB-4j8pd8yEieOM',
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <h1>hello :)</h1>;
 }
 
 export default App;
-
---
+++ src/components/videoList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VideoList = (props) => {};
+
+export default VideoList;
