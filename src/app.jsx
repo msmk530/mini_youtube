3bd22df125b641a45501bb5ec17ea9215c7a20b8
kimminsub <msmk530@naver.com>
2020-10-21 12:42:54 +0900

    [modify] 네트워크 로직 분리
    컴포넌트로 부터 네트워크 통신 로직으르 분리하였다.
    
    컴포넌트는 비즈니스 로직을 갖지 않고 모두 주입받아서 실행되도록 만들어야 유닛 테스트에도 용이하다.

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,26 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import * as config from './config';
 
 import styles from './app.module.css';
 import Header from './components/Header/header';
 import VideoList from './components/VideoList/video_list';
 
-function App() {
-  const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${config.YOUTUBE_API_KEY}`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId })),
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error));
-  };
-
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${config.YOUTUBE_API_KEY}`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
+
   return (
     <div className={styles.app}>
       <Header name={'Youtube'} onSearch={search} />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube';
+import * as config from './config';
+
+const youtube = new Youtube(config.YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
